53287934d10afaeb9608e2f15628bb36370295e3
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2020-08-27 16:10:33 +0200

    apply user project roles on project deletion action

--- routes/projects/delete_project.js
+++ routes/projects/delete_project.js
@@ -1,6 +1,7 @@
 const Boom = require('boom');
 
 const db = require('../../db');
+const authorizationUtils = require('../../authorization_utils');
 
 module.exports = [
   {
@@ -11,22 +12,37 @@ module.exports = [
     handler: function (req, res) {
       const roles = req.auth.credentials.roles || [];
 
-      if (roles.indexOf('edit') === -1) {
-        return res(Boom.unauthorized('Not authorized to perform this action'));
-      }
 
       return db('projects')
+        .select('type') // get project type to check if user authorized to delete this project type.
         .where('id', req.params.id)
-        .del()
-        .then((ret) => {
-          if (ret === 0) {
+        .then(ret => {
+          if (ret.length === 0) {
             return res(Boom.notFound('Could not find the requested project'));
           }
-          return res({id: req.params.id});
-        }).catch(function (err) {
+          if (!authorizationUtils.isAuthorizedEditor(roles, ret[0].type)) {
+            return res(Boom.unauthorized('Not authorized to perform this action'));
+          }
+          return db('projects')
+            .where('id', req.params.id)
+            .del()
+            .then((ret) => {
+              if (ret === 0) {
+                return res(Boom.notFound('Could not find the requested project'));
+              }
+              return res({id: req.params.id});
+            }).catch(function (err) {
+              console.error(err);
+              return res(Boom.badImplementation('Internal Server Error - Could not delete data'));
+            });
+
+        })
+        .catch(function (err) {
           console.error(err);
-          return res(Boom.badImplementation('Internal Server Error - Could not delete data'));
+          return res(Boom.badImplementation('Internal Server Error - Could not find data'));
         });
+
+
     }
   }
 ];
